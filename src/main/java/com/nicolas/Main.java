039133f9fb18039b990bbb780e7ab0f9f3e88ed8
nwiedel <nwiedel@alte-hof-apotheke.com>
2025-01-23 14:53:03 +0100

    First Window

--- src/main/java/com/nicolas/Main.java
+++ src/main/java/com/nicolas/Main.java
@@ -1,7 +1,11 @@
 package com.nicolas;
 
+import com.nicolas.jade.Window;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Window window = Window.get();
+        window.run();
     }
 }
